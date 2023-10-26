c990a9a7fa4cb18af9730814f9f5f1116e39ab48
Prakshenjay Bhati <41756887+Prakshenjay@users.noreply.github.com>
2023-10-26 11:48:55 +0530

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,10 @@
+public class Hello_World {
+    public static void another(String[] args) {
+        System.out.println("Hi again");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        another(args);
+    }
+}
