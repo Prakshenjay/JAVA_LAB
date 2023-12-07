07d6be0202501cee595a03e001795bcf18adfbdd
Prakshenjay Bhati <prakshenjay@gmail.com>
2023-12-07 20:02:55 +0530

    Create ReplaceCharacter.java

---
+++ JAVA_Practice/src/ReplaceCharacter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+    	
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter a String : ");
+        String str = scn.nextLine();
+
+        System.out.print("Enter character to replace : ");
+        char x = scn.next().charAt(0);
+        System.out.print("Enter new character : ");
+        char y = scn.next().charAt(0);
+
+        str.replace(x,y);
+
+        System.out.println("Modified string is : " + str);
+        
+        scn.close();
+
+        }
+    }
